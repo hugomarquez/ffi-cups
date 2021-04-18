f67b634a4b4dc185234d2614fd6915cc3e0d950a
Hugo Marquez <hugomarquez.dev@gmail.com>
2021-04-17 20:51:55 -0500

    adds printer attribute to job class

--- lib/ffi-cups/job.rb
+++ lib/ffi-cups/job.rb
@@ -1,13 +1,11 @@
 module Cups
   class Job
-    attr_accessor :id, :title, :printer
-
+    attr_reader :id, :title, :printer
     def initialize(id, title, printer=nil)
       @id = id
       @title = title
       @printer = printer
     end
-
   end
 end
 
